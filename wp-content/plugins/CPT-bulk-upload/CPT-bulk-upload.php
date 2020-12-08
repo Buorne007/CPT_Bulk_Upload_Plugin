6675113cd2bae18b8fb92a7c4f52c53aece4bc16
Christopher Igwubor <kristofa@MBPdiChistopher.lan>
2020-12-09 00:58:27 +0100

    Initial commit

---
+++ wp-content/plugins/CPT-bulk-upload/CPT-bulk-upload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package CPT Bulk Upload
+ */
+/*
+Plugin Name: CPT Bulk Upload
+Plugin URI:
+Description: This plugin provides the functionality to upload custom post types in bulk. Custom ACF fields are also supported
+Version: 1.0.0
+Author: Christopher Igwubor
+Author URI:
+License: GPLv2 (or later)
+Text Domain: CPT-bulk-upload
+ */
